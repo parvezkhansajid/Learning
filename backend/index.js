93a27bbd1a85c5b326377890cb00fb4b481b3e6b
parvezkhansajid <parvezkhansajid@gmail.com>
2024-04-30 01:05:17 +0600

    Manual Routing using if/else

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res, next) => {
-  res.end("<h1>Hello From Server</h1>");
+  if (req.url == "/") {
+    res.end("<h1>Home Page</h1>");
+  } else if (req.url == "/about") {
+    res.end("<h1>About Page</h1>");
+  } else if (req.url == "/contact") {
+    res.end("<h1>Contact Page</h1>");
+  } else {
+    res.end("<h1>Page Not Found</h1>");
+  }
 });
 
 const port = 2711;
