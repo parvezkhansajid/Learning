9d31af9569b149ece9de9a7ab3640d1374453c27
parvezkhansajid <parvezkhansajid@gmail.com>
2024-04-30 18:19:11 +0600

    Created a Login Form

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,34 @@ import path from "path";
 
 const app = express();
 
+const users = [];
+
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(path.resolve(), "public")));
 
+app.use(express.urlencoded({ extended: true }));
 
 const port = 2711;
 
 app.get("/", (req, res, next) => {
-  res.render("index.ejs", { name: "Sajid" });
+  res.render("index.ejs");
+});
+
+app.get("/success", (req, res, next) => {
+  res.render("success");
+});
+
+app.post("/", (req, res, next) => {
+  users.push({ username: req.body.name, email: req.body.email });
+  // res.render("success");
+  res.redirect("/success");
+});
+
+app.get("/users", (req, res, next) => {
+  res.json({
+    users,
+  });
 });
 
 app.listen(port, () => {
