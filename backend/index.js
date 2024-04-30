3bec3680ae2edbb696e18c684fa7477a74245da3
parvezkhansajid <parvezkhansajid@gmail.com>
2024-04-30 17:51:50 +0600

    Serving Static File using express.static()

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+
 app.set("view engine", "ejs");
+
+app.use(express.static(path.join(path.resolve(), "public")));
+
+
 const port = 2711;
 
 app.get("/", (req, res, next) => {
-  res.render("index.ejs", {name: "Sajid"});
+  res.render("index.ejs", { name: "Sajid" });
 });
 
 app.listen(port, () => {
